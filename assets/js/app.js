a4e11d37c64677965d8bd02581838e15f23c2351
neetuktoor <neetuktoor@gmail.com>
2017-07-06 00:25:47 -0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,48 @@
 var animalArray = ["dog", "cat", "panda", "giraffe", "squirrel", "zebra", "elephant", "lion", "deer", "rabbit"];
 var apiKey = "d717d9d1126c4211b02b4fd33716203c";
 var rating = "pg";
-var limit = 10;
+// var limit = 10;
+
+// Function for rendering buttons for each animal in the array
+      function renderButtons() {
+
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#gifs-appear-here").empty();
+
+        //looping through the array of animals
+        for (var i = 0; i < animalArray.length; i++) {
+
+          // Then dynamicaly generating buttons for each animal in the array
+          var a = $("<button>");
+          // Adding a class of animal to our button
+          a.addClass("animalButton");
+          // Adding a data-attribute
+          a.attr("data-name", animalsArray[i]);
+          // Providing the initial button text
+          a.text(animalsArray[i]);
+          // Adding the button to the buttons-view div
+          $("#gifs-appear-here").append(a);
+        }
+      }
+
+//add additional animals to the array
+      $("#add-animal").on("click", function(event) {
+        event.preventDefault();
+        var animal = $("#animal-input").val().trim();
+        $("#animal-input").val("");
+        animals.push(animal);
+        renderButtons();
+      });
+
+
 
 // displayAnimals function re-renders the HTML to display the appropriate content
       function displayAnimals() {
 
         var animal = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "api_key=d717d9d1126c4211b02b4fd33716203c&limit=10&lang=en";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+        // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "api_key=d717d9d1126c4211b02b4fd33716203c&limit=10&lang=en";
       
         // Performing our AJAX GET request      
           $.ajax({
@@ -22,6 +57,8 @@ var limit = 10;
             var results = response.data;
 
             // Looping over every result item
+            $("#gifPanel").empty();
+            
             for (var i = 0; i < results.length; i++) {
 
             var gifDiv = $("<div class='item'>");
@@ -41,25 +78,3 @@ var limit = 10;
         });
       }
   
-// Function for rendering buttons for each animal in the array
-      function renderButtons() {
-
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-
-        //looping through the array of animals
-        for (var i = 0; i < animalArray.length; i++){
-
-          // Then dynamicaly generating buttons for each animal in the array
-          var a = $("<button>");
-          // Adding a class of animal to our button
-          a.addClass("animal");
-          // Adding a data-attribute
-          a.attr("data-name", animals[i]);
-          // Providing the initial button text
-          a.text(animals[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
-
